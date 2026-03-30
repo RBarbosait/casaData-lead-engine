5ba12c9ceb43fe6249833f6e4786beea5424ea9b
Rodrigo Barbosa <rbarbosait@gmail.com>
2026-03-30 20:12:54 -0300

    fix: edge runtime

--- app/api/property/[id]/route.ts
+++ app/api/property/[id]/route.ts
@@ -1,7 +1,6 @@
-import { prisma } from "@/lib/prisma";
+export const runtime="edge"
 
-export const runtime = 'edge'
-export const dynamic = "force-dynamic";
+import { prisma } from "@/lib/prisma";
 
 export async function GET(
   req: Request,
