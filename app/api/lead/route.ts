93f35d996b7b379b4f0c3533e97e4f26be281924
Rodrigo <113548465+RBarbosait@users.noreply.github.com>
2026-03-31 14:19:54 -0300

    Update route.ts

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -1,6 +1,4 @@
-//import { prisma } from "@/lib/prisma";
-
-export const runtime = 'edge'
+import { prisma } from "@/lib/prisma";
 export async function POST(req: Request) {
 try {
 const body = await req.json();
